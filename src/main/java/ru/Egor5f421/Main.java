0b7edbc0dba64c37cbe0d5328efd957ce77fea1c
Егор <Egor5f421@main.ru>
2023-12-16 14:40:32 +0300

    Добавлен фон главной программы

--- src/main/java/ru/Egor5f421/Main.java
+++ src/main/java/ru/Egor5f421/Main.java
@@ -1,13 +1,7 @@
 package ru.Egor5f421;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.geom.Ellipse2D;
-import java.io.File;
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 public class Main extends JFrame {
 
@@ -28,40 +22,38 @@ public class Main extends JFrame {
     /** Отрисовывает всю графику
      * @see #Main(String, int, int)  Main */
     private void window() {
+        var f = this;
         final JPanel panel = new JPanel(new GridBagLayout()) {
             @Override
             public void paint(Graphics g) {
-//                Background
+                //Background
                 Graphics2D d = (Graphics2D) g;
                 d.setColor(new Color(0,255,127));
-                Shape bg = new Ellipse2D.Double(-300,-300,1500,1500);
-                d.fill(bg);
-                d.draw(bg);
-                d.setColor(Color.BLACK);
+                d.fillRect(0,0,getWidth(),getHeight());
 
-                int y = 300;
-                int w = 70;
-                int h = 20;
+                d.setColor(new Color(231, 66, 101));
+                d.setFont(new Font("font", Font.BOLD, 50));
+                d.drawString("Фон", 195,getHeight()/2-getHeight()/4);
 
-                int ox = 80;
-                int c = 3;
-                int o = 130;
-                int j = ox + (o * c);
-                for (int i = ox; i < j; i+= o) {
-                    d.fillRoundRect(i,y,w,h,10,20);
+                d.setStroke(new BasicStroke(10));
+                d.drawRoundRect(195, getHeight()/2-getHeight()/4+10, 105,1,5,1);
+                try {
+                    String patch = System.getProperty("user.dir")+"/src/main/resources/E.png";
+                    d.drawImage(new ImageIcon(patch).getImage(), 195, 180, 100, 170, super::imageUpdate);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
                 }
-
-                d.drawImage();
             }
         };
         getContentPane().add(panel);
-        /*final GridBagConstraints grid = new GridBagConstraints();
-        panel.setBackground(new Color(0, 255, 127));
+
+        final GridBagConstraints grid = new GridBagConstraints();
 
         grid.gridx = 0;
         grid.gridy = 0;
-        grid.gridwidth = 2;
-        grid.gridheight = 1;*/
+        grid.gridwidth = 3;
+        grid.gridheight = 1;
+
     }
 
     public static void main(String[] args) {
